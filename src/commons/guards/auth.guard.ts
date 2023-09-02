307afad796c0dd9b4bf29a5ed280a71d5b274507
gabriel-victor933 <gabrielvictor933@usp.br>
2023-09-02 18:21:15 -0300

    feat: user guards autentication

---
+++ src/commons/guards/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(private readonly jwt: JwtService){}
+    async canActivate(context: ExecutionContext) {
+        const request = context.switchToHttp().getRequest();
+        const token = request.headers.authorization?.split(" ")[1]
+        if(!token) throw new UnauthorizedException("missing token")
+        try {
+            const payload = await this.jwt.verifyAsync(token,{secret: "top_secret"})
+            request['user'] = payload.userId
+        } catch(err){
+            throw new UnauthorizedException("Invalid token");
+        }
+        return true
+    }
+}
